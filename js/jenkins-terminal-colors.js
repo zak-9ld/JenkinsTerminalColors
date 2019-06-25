dabab7c73ed6e9a929f4fa2a4d8c5e5ad3fb5845
Michal Zak <michal.zak@dotdata.com>
2019-06-24 17:45:38 -0700

    Treat 0;39m as </span>

--- js/jenkins-terminal-colors.js
+++ js/jenkins-terminal-colors.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     function transformText(text) {
         text = text.replace(/\[0m/g, "</span>");
+        text = text.replace(/\[0;39m/g, "</span>");
         text = text.replace(/\[([0-9;]+)m/g, function (match, code, offset, string) {
             return "<span class='jtc-" + code.replace(/;/g, ' jtc-') + "'>";
         });
